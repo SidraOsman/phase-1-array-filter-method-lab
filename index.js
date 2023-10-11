7073b2d8460df7886662f229994136a4b639008f
SidraOsman <osmansidra4@gmail.com>
2023-10-11 20:06:39 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+const drivers = ['Bobby' , 'Sammy' , 'Sally' , 'Annette' , 'Sarah' , 'bobby'];
+const searchName = 'Bobby';
+function findMatching(drivers , searchName){
+    return drivers.filter(driver => driver.toLowerCase() === searchName.toLowerCase());
+}
+
+function fuzzyMatch(drivers , query){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+
+}
+
+const driversHomeTowns = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(drivers , searchName){
+    return drivers.filter(driver => driver.name.toLowerCase() === searchName.toLowerCase());
+  }
